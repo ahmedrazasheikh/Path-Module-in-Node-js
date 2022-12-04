fb40ab8a15072afaa17f78babd5c64c6abf6af7b
Ahmed Raza <bhaia9036@gmail.com>
2022-12-04 14:15:53 -0800

    initaial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var path = require("path");
+// The Path module provides a way of working with directories and file paths.
+
+
+// console.log(__filename)
+// console.log(__dirname)
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+// console.log(path.dirname(__filename))
+
+// console.log(path.parse(__filename))
+// console.log(path.parse(__filename).name)
+
+// win32
